987d01c84dff03dc4b0b036bc227fdc9d52578bc
hwang-fu <junzhe.hwangfu@gmail.com>
2025-12-07 19:38:58 +0100

    feat: add command-line argument handling

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,15 @@
 module Main where
 
+import System.Environment (getArgs, getProgName)
+import System.Exit (exitFailure)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  args <- getArgs
+  case args of
+    [filepath] -> do
+      putStrLn $ "The file to validate is: " ++ filepath
+    _ -> do
+      progName <- getProgName
+      putStrLn $ "Usage: " ++ progName ++ " <filepath>"
+      exitFailure
